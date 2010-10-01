694a210ecbf7178da6daff0fe80b592194780bf9
Marco Tabini <marcot@tabini.ca>
2010-09-30 20:49:21 -0400

    - Initial commit

---
+++ Classes/AFKLoadManagerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AFKLoadManagerAppDelegate.h
+//  AFKLoadManager
+//
+//  Created by Marco Tabini on 10-09-30.
+//  Copyright 2010 AFK Studio Partnership. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFKLoadManagerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
